5f795954fc2a8359953b2481b94193a2a63e46d6
KjAnimates <kjhouser43@gmail.com>
2023-11-27 06:10:57 -0500

    Added home page & PORT constant.

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const app = express();
+const PORT = 5000;
 
 app.use(express.static(__dirname + "/src/public"));
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     console.log("Started Dev server.");
-    console.log("  > Localhost:5000");
+    console.log("  > Localhost:%s", PORT);
 })
 
-app.get('/', (req, res) => {
+app.get("/home", (req, res) => {
     res.sendFile(__dirname + "/src/public/index.html")
+})
+
+// Redirect to the home page;
+app.get('/', (req, res) => {
+    res.redirect("home")
 })
